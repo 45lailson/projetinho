cefc7dd8fa400d413197646b8ed4811c55c72d3f
Lailson Sousa <lailson4545@hotmail.com>
2023-05-16 22:23:58 -0300

    adicionando rotas e melhorando a mesma.

--- app/router/router.php
+++ app/router/router.php
@@ -8,17 +8,46 @@ function routes()
 function exactMatchUriRoutes($uri, $routes)
 {
     if (array_key_exists($uri,$routes)) {
-        return [];
+        return [$uri => $routes[$uri]];
     } else {
        return [];
 
     }
 }
+
+function regularExpressionArrayRoutes($uri,$routes)
+{
+    return  array_filter(
+        $routes,
+        function ($value) use ($uri) {
+            $regex = str_replace('/','\/',ltrim($value,'/'));
+//                    return preg_match("/^$regex$/",ltrim($uri,'/'));
+            return $regex;
+        },
+        ARRAY_FILTER_USE_KEY
+    );
+}
+
+function params($uri, $matchedUri)
+{
+    if (!empty($matchedUri)) {
+        $matchedToParams = array_keys($matchedUri)[0];
+
+    }
+}
+
+//8:30 parei na aula
+
 function router()
 {
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $routes = routes();
     $matchedUri = exactMatchUriRoutes($uri,$routes);
+    if (!empty($matchedUri)) {
+        $matchedUri = regularExpressionArrayRoutes($uri,$routes);
 
+    }
+
+    var_dump($matchedUri);
 
 }
--- app/router/routes.php
+++ app/router/routes.php
@@ -2,7 +2,9 @@
 
 return [
     '/' => 'Home@index',
-    '/user/create' => 'User@create'
+    '/user/create' => 'User@create',
+    '/user/[a-z0-9]+' =>'User@index',
+    '/user/[a-z0-9]+/name/[a-z]+' =>'User@show'
 ];
 
 
